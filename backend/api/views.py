48face51a4a99e2d4de21e5577a9bf0d718ac8df
marco <marco.prina01@universitadipavia.it>
2020-09-08 17:48:19 +0200

    Provato a loggare eventuale errori

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import os
 
 from django.db import transaction
@@ -14,6 +15,8 @@ from django.core.files.storage import default_storage
 
 from backend.api.serializers import CorsoSerializer, LezioneSerializer
 from backend.models import Corsi
+
+logger = logging.getLogger(__name__)
 
 
 class NewCorso(APIView):
@@ -46,30 +49,37 @@ class NewLezione(APIView):
 
         serializer = LezioneSerializer(data=input_data)
         if serializer.is_valid():
-            serializer.save()
+            AnalyzeVideo(video_name, serializer).start()
 
-            AnalyzeVideo(video_name, serializer.data).start()
-
-            data = serializer.data
-            data['message'] = 'Analyzing video'
-            return Response(data, status=status.HTTP_201_CREATED)
+            return Response('Analyzing video', status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class AnalyzeVideo(threading.Thread):
+
+
     def __init__(self, video_name, lezione):
         threading.Thread.__init__(self)
         self.video_name = video_name
         self.lezione = lezione
 
+    # @transaction.atomic()
     def run(self):
-        # try:
-        ParseVideo(self.lezione) \
-            .getCaptionFromFile('Outputs/prova/caption.txt')\
-            .parseFromCaption(posTag=['S', 'A'])
-        #    .getCaptionFromVideo(self.video_name, 'backend/YoutubeAPI/credentials.json') \
-        #    .parseFromCaption(posTag=['S', 'A'])
+        try:
+            with transaction.atomic():
+                self.lezione.save()
+                ParseVideo(self.lezione.data) \
+                    .getCaptionFromFile('Outputs/prova/caption.txt') \
+                    .parseFromCaption(posTag=['S', 'A'])
+                #    .getCaptionFromVideo(self.video_name, 'backend/YoutubeAPI/credentials.json') \
+                #    .parseFromCaption(posTag=['S', 'A'])
+
+            # segnare video come "elaborato"
+        except Exception as e:
+            logger.error('Error analyzing video of "%s" corso in "%s" lesson',
+                          self.lezione.data['corso'],
+                          self.lezione.data['nome'],
+                          exc_info=e)
+
         os.remove(self.video_name)
         # os.remove(self.video_name.replace("Video", "Audio", 1) + '.flac')
-        # segnare video come "elaborato"
-        # except Exception:
