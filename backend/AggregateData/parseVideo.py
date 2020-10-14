f08d29f14e37c1740157900406ecd6607671be2f
marco <marco.prina01@universitadipavia.it>
2020-10-14 17:18:48 +0200

    Rinominate cartelle e file

--- backend/AggregateData/parseVideo.py
+++ backend/AggregateData/parseVideo.py
@@ -6,16 +6,15 @@ from urllib.parse import parse_qs
 
 from django.db import transaction
 
-from backend.AggregateData.aggregateVideos import AggregateVideos
 from backend.AggregateData.cropCaption import CropCaption
 from backend.AggregateData.findBinomi import FindBinomi
 from backend.AggregateData.findwords import FindWords
 from backend.AggregateData.lda import LDA
 from backend.AggregateData.tokenize import Tokenize
-from backend.YoutubeAPI.captionDownload import CaptionDownload
-from backend.YoutubeAPI.credentials import YoutubeCredentials
-from backend.YoutubeAPI.speech2text import Speech2Text
-from backend.YoutubeAPI.video2audio import Video2audio
+from backend.ExternalAPI.captionDownload import CaptionDownload
+from backend.ExternalAPI.youtubeCredentials import YoutubeCredentials
+from backend.ExternalAPI.speech2text import Speech2Text
+from backend.AggregateData.video2audio import Video2audio
 
 
 class ParseVideo:
