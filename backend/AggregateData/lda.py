5b18dcf0297eaf97f27ced46cd83fba3fd9320b2
marco <marco.prina01@universitadipavia.it>
2020-11-07 11:12:53 +0100

    Aggiunto codice commentato per visualizzare grafici coherence score

--- backend/AggregateData/lda.py
+++ backend/AggregateData/lda.py
@@ -1,7 +1,10 @@
 import gensim
+import pyLDAvis.gensim
+import pickle
+import pyLDAvis
 from gensim import corpora
 from gensim.models import CoherenceModel
-
+import matplotlib.pyplot as plt
 from backend.models import LdaCorso, LdaLezione
 
 
@@ -27,7 +30,22 @@ class LDA():
         dictionary = corpora.Dictionary(sentences)
         corpus = [dictionary.doc2bow(text) for text in sentences]
 
-        # self.ldamodel = gensim.models.ldamodel.LdaModel(self.corpus, num_topics=nTopic, id2word=self.dictionary, passes=15)
+        '''
+        model_list, coherence_values = self.coherence_values_computation(
+            dictionary=dictionary, corpus=corpus, texts=sentences,
+            start=1, limit=40, step=2
+        )
+        limit = 40
+        start = 1
+        step = 2
+        x = range(start, limit, step)
+        plt.plot(x, coherence_values)
+        plt.xlabel("Num Topics")
+        plt.ylabel("Coherence score")
+        plt.legend(("coherence_values"), loc='best')
+        plt.show()
+        print('ok')
+        '''
 
         self.ldamodel = gensim.models.wrappers.LdaMallet(self.mallet_path, corpus=corpus, num_topics=nTopic,
                                                          id2word=dictionary)
@@ -39,13 +57,29 @@ class LDA():
         #     self.ldamodel.log_perplexity(self.corpus))  # a measure of how good the model is. lower the better.
 
         # Compute Coherence Score
-        coherence_model_lda = CoherenceModel(model=self.ldamodel, texts=sentences, coherence='c_v')
+        coherence_model_lda = CoherenceModel(model=self.ldamodel, dictionary=dictionary, texts=sentences, coherence='c_v')
         coherence_lda = coherence_model_lda.get_coherence()
+
         print('\nCoherence Score: ', coherence_lda, '\r\n')
 
-    '''def display(self):
+    def display(self):
         vis = pyLDAvis.gensim.prepare(self.ldamodel, self.corpus, self.dictionary)
-        pyLDAvis.show(vis)'''
+        pyLDAvis.show(vis)
+
+    def coherence_values_computation(self, dictionary, corpus, texts, limit, start=2, step=3):
+        coherence_values = []
+        model_list = []
+        for num_topics in range(start, limit, step):
+            model = gensim.models.wrappers.LdaMallet(
+                self.mallet_path, corpus=corpus, num_topics=num_topics, id2word=dictionary
+            )
+            model_list.append(model)
+            coherencemodel = CoherenceModel(
+                model=model, texts=texts, dictionary=dictionary, coherence='c_v'
+            )
+            coherence_values.append(coherencemodel.get_coherence())
+
+        return model_list, coherence_values
 
     def saveOnDBLezione(self, lezione):
         LdaLezione.objects.filter(lezione=lezione).delete()
